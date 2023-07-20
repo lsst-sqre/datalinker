14e13d866e50f1ee529aca9f33e55bff488d1761
Christine Banek <cbanek@lsst.org>
2023-07-19 17:00:32 -0700

    [DM-39905] Have tests work for s3

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,15 +2,18 @@
 
 from __future__ import annotations
 
+import os
 from collections.abc import AsyncIterator, Iterator
 from datetime import timedelta
 from pathlib import Path
 
+import boto3
 import pytest
 import pytest_asyncio
 from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from httpx import AsyncClient
+from moto import mock_s3
 from safir.testing.gcs import MockStorageClient, patch_google_storage
 
 from datalinker import main
@@ -45,10 +48,28 @@ def mock_butler() -> Iterator[MockButler]:
     yield from patch_butler()
 
 
-@pytest.fixture
+@pytest.fixture(scope="function")
+def aws_credentials() -> None:
+    """Mocked AWS Credentials for moto."""
+    os.environ["AWS_ACCESS_KEY_ID"] = "testing"
+    os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
+    os.environ["AWS_SECURITY_TOKEN"] = "testing"
+    os.environ["AWS_SESSION_TOKEN"] = "testing"
+    os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+
+@pytest.fixture(scope="function")
+def s3(aws_credentials: None) -> boto3.client:
+    with mock_s3():
+        yield boto3.client("s3", region_name="us-east-1")
+
+
+@pytest.fixture(scope="function")
 def mock_google_storage() -> Iterator[MockStorageClient]:
     """Mock out the Google Cloud Storage API."""
+    config.google_credentials = "creds.ini"
     yield from patch_google_storage(
         expected_expiration=timedelta(hours=1),
         bucket_name="some-bucket",
     )
+    config.google_credentials = ""
