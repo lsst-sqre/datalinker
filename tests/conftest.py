0eaf0e92b6f54d0a7677dcf3982bde2fa8ff6706
Christine Banek <cbanek@lsst.org>
2023-07-21 13:21:11 -0700

    [DM-39905] Clear AWS environment variables after each test

--- tests/conftest.py
+++ tests/conftest.py
@@ -49,13 +49,20 @@ def mock_butler() -> Iterator[MockButler]:
 
 
 @pytest.fixture
-def aws_credentials() -> None:
+def aws_credentials() -> Iterator[None]:
     """Mocked AWS Credentials for moto."""
     os.environ["AWS_ACCESS_KEY_ID"] = "testing"
     os.environ["AWS_SECRET_ACCESS_KEY"] = "testing"
     os.environ["AWS_SECURITY_TOKEN"] = "testing"
     os.environ["AWS_SESSION_TOKEN"] = "testing"
     os.environ["AWS_DEFAULT_REGION"] = "us-east-1"
+
+    yield
+
+    del os.environ["AWS_ACCESS_KEY_ID"]
+    del os.environ["AWS_SECRET_ACCESS_KEY"]
+    del os.environ["AWS_SECURITY_TOKEN"]
+    del os.environ["AWS_DEFAULT_REGION"]
 
 
 @pytest.fixture
