93549954b9c8ba012e5b3259df37e5ef2f071039
Russ Allbery <rra@lsst.org>
2026-07-06 17:21:12 -0700

    Switch the build system to nox
    This is the new standard for SQuaRE projects. It's necessary for
    vertical monorepo repositories, and it seems better to use the same
    tool everywhere.

---
+++ noxfile.py
@@ -0,0 +1,47 @@
+"""nox configuration for datalinker."""
+
+import nox
+from nox_uv import session
+
+# Default sessions.
+nox.options.sessions = ["lint", "typing", "test"]
+
+# Other nox defaults.
+nox.options.default_venv_backend = "uv"
+nox.options.reuse_existing_virtualenvs = True
+
+
+@session(name="coverage-report", uv_groups=["dev"])
+def coverage_report(session: nox.Session) -> None:
+    """Generate a code coverage report from the test suite."""
+    session.run("coverage", "report", *session.posargs)
+
+
+@session(uv_only_groups=["lint"], uv_no_install_project=True)
+def lint(session: nox.Session) -> None:
+    """Run pre-commit hooks."""
+    session.run("pre-commit", "run", "--all-files", *session.posargs)
+
+
+@session(uv_groups=["dev"])
+def test(session: nox.Session) -> None:
+    """Test the Semaphore server."""
+    session.run(
+        "pytest",
+        "--cov=datalinker",
+        "--cov-branch",
+        "--cov-report=",
+        *session.posargs,
+    )
+
+
+@session(uv_groups=["dev", "typing"])
+def typing(session: nox.Session) -> None:
+    """Run mypy."""
+    session.run(
+        "mypy",
+        *session.posargs,
+        "noxfile.py",
+        "src",
+        "tests",
+    )
