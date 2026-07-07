1e87231e2670de7a3a95bc0fcf45e3f1f0bb9c8a
Russ Allbery <rra@lsst.org>
2026-07-07 13:47:51 -0700

    Use the new safir.testing.data library
    Rearrange test files somewhat and use the safir.testing.data library
    to locate files. Further work will be needed to convert all the
    tests to use externalized data.

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,6 +10,7 @@ from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
 from httpx import ASGITransport, AsyncClient
 from rubin.repertoire import Discovery, register_mock_discovery
+from safir.testing.data import Data
 
 from datalinker import main
 from datalinker.config import config
@@ -17,15 +18,25 @@ from datalinker.config import config
 from .support.butler import MockButler, patch_butler
 
 
+def pytest_addoption(parser: pytest.Parser) -> None:
+    parser.addoption(
+        "--update-test-data",
+        action="store_true",
+        default=False,
+        help="Overwrite expected test output with current results",
+    )
+
+
 @pytest_asyncio.fixture
-async def app(monkeypatch: pytest.MonkeyPatch) -> AsyncGenerator[FastAPI]:
+async def app(
+    data: Data, monkeypatch: pytest.MonkeyPatch
+) -> AsyncGenerator[FastAPI]:
     """Return a configured test application.
 
     Wraps the application in a lifespan manager so that startup and shutdown
     events are sent during test execution.
     """
-    test_data_path = Path(__file__).parent / "data"
-    monkeypatch.setattr(config, "tap_metadata_dir", test_data_path)
+    monkeypatch.setattr(config, "tap_metadata_dir", data.path("metadata"))
     async with LifespanManager(main.app):
         yield main.app
 
@@ -46,6 +57,12 @@ async def client(app: FastAPI) -> AsyncGenerator[AsyncClient]:
 
 
 @pytest.fixture
+def data(request: pytest.FixtureRequest) -> Data:
+    update = request.config.getoption("--update-test-data")
+    return Data(Path(__file__).parent / "data", update_test_data=update)
+
+
+@pytest.fixture
 def mock_butler() -> Iterator[MockButler]:
     """Mock Butler for testing."""
     yield from patch_butler()
@@ -53,8 +70,7 @@ def mock_butler() -> Iterator[MockButler]:
 
 @pytest.fixture(autouse=True)
 def mock_discovery(
-    respx_mock: respx.Router, monkeypatch: pytest.MonkeyPatch
+    data: Data, respx_mock: respx.Router, monkeypatch: pytest.MonkeyPatch
 ) -> Discovery:
     monkeypatch.setenv("REPERTOIRE_BASE_URL", "https://example.com/repertoire")
-    path = Path(__file__).parent / "data" / "discovery.json"
-    return register_mock_discovery(respx_mock, path)
+    return register_mock_discovery(respx_mock, data.path("discovery.json"))
